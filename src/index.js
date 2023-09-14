ac1fe924c1f1134ceb0e67b7c881098771246cfe
AnastasiiaPavlovska <nastiapavlovska446@gmail.com>
2023-09-14 13:09:38 +0200

    fix build

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <ToastContainer
-      position={'top-right'}
-      autoClose={3000}
-      hideProgressBar={false}
-      closeOnClick={true}
-      pauseOnHover={true}
-      draggable={true}
-      progress={undefined}
-      theme={'light'}
-    />
   </React.StrictMode>
 );
 
